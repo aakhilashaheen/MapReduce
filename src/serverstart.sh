b18192316be9d506625aaed75daaf13fb3023df1
Shannon Sequeira <sequeira@umn.edu>
2019-03-06 19:08:22 -0600

    Start script runs 4 nodes

--- src/serverstart.sh
+++ src/serverstart.sh
@@ -6,6 +6,8 @@ rm -rf *.class
 javac -cp ".:/usr/local/Thrift/*" ServerHandler.java -d . &&  javac -cp ".:/usr/local/Thrift/*" Client.java -d . && javac -cp ".:/usr/local/Thrift/*" WorkerHandler.java -d .
 gnome-terminal -e "java -cp \".:/usr/local/Thrift/*\" ServerHandler 9090 1"
 gnome-terminal -e "java -cp \".:/usr/local/Thrift/*\" WorkerHandler localhost 9090 9091 0.2"
+gnome-terminal -e "java -cp \".:/usr/local/Thrift/*\" WorkerHandler localhost 9090 9092 0.2"
+gnome-terminal -e "java -cp \".:/usr/local/Thrift/*\" WorkerHandler localhost 9090 9093 0.2"
 gnome-terminal -e "java -cp \".:/usr/local/Thrift/*\" WorkerHandler localhost 9090 9094 0.2"
 sleep 3s
 java -cp ".:/usr/local/Thrift/*" Client localhost 9090 input_dir
